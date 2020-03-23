84844ef60a0e348b2efd886676314c7090683ace
Michał Kalbarczyk <fazibear@gmail.com>
2020-03-23 22:30:38 +0100

    Kernel: Add nice name instead of "Hello world"

--- kernel/main.c
+++ kernel/main.c
@@ -2,8 +2,36 @@
 
 void _main() {
   screen_cls();
-  screen_println("Hello World!");
+  
+  screen_println("");
+  
+  screen_print("                           ");
   screen_set_color(YELLOW);
-  screen_set_background_color(BLUE);
-  screen_println("Hello World!");
+  screen_print("* ");
+  screen_set_color(BLUE);
+  screen_print("* ");
+  screen_set_color(RED);
+  screen_print("* ");
+  screen_set_color(GREEN);
+  screen_print("* ");
+  screen_set_color(GREY);
+  screen_print("* ");
+  
+  screen_set_color(WHITE);
+  screen_print("FAZOS ");
+  
+  screen_set_color(GREY);
+  screen_print("* ");
+  screen_set_color(GREEN);
+  screen_print("* ");
+  screen_set_color(RED);
+  screen_print("* ");
+  screen_set_color(BLUE);
+  screen_print("* ");
+  screen_set_color(YELLOW);
+  screen_println("*");
+  
+  screen_println("");
+  screen_set_color(WHITE);
+  screen_println("READY.");
 }
