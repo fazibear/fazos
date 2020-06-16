f8c8f6573f38f1bce12fc3cb9224db0508f15c56
Michał Kalbarczyk <fazibear@gmail.com>
2020-06-16 22:54:03 +0200

    Kernel: Make paging more verbose

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -3,7 +3,7 @@
 
 
 unsigned long paging_page_directory[1024] __attribute__((aligned(4096)));
-unsigned long paging_first_page_table[1024] __attribute__((aligned(4096)));
+unsigned long paging_page_table_1[1024] __attribute__((aligned(4096)));
 
 void paging_init() {
   INIT_START();
@@ -13,21 +13,25 @@ void paging_init() {
     //   Supervisor: Only kernel-mode can access them
     //   Write Enabled: It can be both read from and written to
     //   Not Present: The page table is not present
-    paging_page_directory[i] = 0x00000002;
+    paging_page_directory[i] = paging_entry(0x0, PAGING_PAGE_DIRECTORY_FLAG_WRITE);
   }
 
   //we will fill all 1024 entries in the table, mapping 4 megabytes
   for(int i = 0; i < 1024; i++) {
     // As the address is page aligned, it will always leave 12 bits zeroed.
     // Those bits are used by the attributes ;)
-    paging_first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+    paging_page_table_1[i] = paging_entry(i * 0x1000, PAGING_PAGE_TABLE_FLAG_WRITE | PAGING_PAGE_TABLE_FLAG_PRESENT);
   }
 
   // attributes: supervisor level, read/write, present
-  paging_page_directory[0] = ((unsigned int)paging_first_page_table) | 3;
+  paging_page_directory[0] = paging_entry((unsigned int)paging_page_table_1, PAGING_PAGE_DIRECTORY_FLAG_WRITE | PAGING_PAGE_DIRECTORY_FLAG_PRESENT);
 
   paging_load_page_directory((unsigned int)paging_page_directory);
   paging_enable();
 
   INIT_END();
+}
+
+unsigned long paging_entry(unsigned long address, short flags) {
+  return (address &= ~0b111111111111) | flags;
 }
--- src/kernel/paging.h
+++ src/kernel/paging.h
@@ -1,8 +1,29 @@
 #ifndef _PAGING_H
 #define _PAGING_H
 
+#define PAGING_PAGE_DIRECTORY_FLAG_IGNORED 1 << 8
+#define PAGING_PAGE_DIRECTORY_FLAG_PAGE_SIZE 1 << 6
+#define PAGING_PAGE_DIRECTORY_FLAG_ACCESSED 1 << 5
+#define PAGING_PAGE_DIRECTORY_FLAG_CACHE_DISABLED 1 << 4
+#define PAGING_PAGE_DIRECTORY_FLAG_WRITE_THROUGH 1 << 3
+#define PAGING_PAGE_DIRECTORY_FLAG_USER 1 << 2
+#define PAGING_PAGE_DIRECTORY_FLAG_WRITE 1 << 1
+#define PAGING_PAGE_DIRECTORY_FLAG_PRESENT 1 << 0
+
+#define PAGING_PAGE_TABLE_FLAG_GLOBAL 1 << 8
+#define PAGING_PAGE_TABLE_FLAG_DIRTY 1 << 6
+#define PAGING_PAGE_TABLE_FLAG_ACCESSED 1 << 5
+#define PAGING_PAGE_TABLE_FLAG_CACHE_DISABLED 1 << 4
+#define PAGING_PAGE_TABLE_FLAG_WRITE_THROUGH 1 << 3
+#define PAGING_PAGE_TABLE_FLAG_USER 1 << 2
+#define PAGING_PAGE_TABLE_FLAG_WRITE 1 << 1
+#define PAGING_PAGE_TABLE_FLAG_PRESENT 1 << 0
+
 extern void paging_load_page_directory(unsigned int);
 extern void paging_enable();
+
+void paging_init();
+unsigned long paging_entry(unsigned long address, short flags);
 
 void paging_init();
 
